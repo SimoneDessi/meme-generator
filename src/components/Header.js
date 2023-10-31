ff898b06e18dc39f4d0bb53ab855fdc6f18bcb28
SimoneDessi <simonedessi1984@gmail.com>
2023-10-31 11:28:45 +0000

    add to githiub

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Header() {
+  return (
+  <div className="header--container">
+    <img src="../images/logo.png" alt="" className="logo" /> 
+    <h2 className="header--title">Meme Generator</h2>
+    <p className="header--project">React course -  project 3</p>
+     </div>
+)}
